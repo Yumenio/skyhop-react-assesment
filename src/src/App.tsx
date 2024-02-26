62dd3bf7b234384a84584b2f22630aa9b105c338
Yumenio <alejandroandy447@gmail.com>
2024-02-26 13:47:06 -0500

    fix: formats all files with prettier to meet the standard

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import './App.css';
-import FileUploader from './components/FileUploader';
+import React from 'react'
+import './App.css'
+import FileUploader from './components/FileUploader'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Document Upload</h1>
-      <FileUploader/>
-    
+    <div className='App'>
+      <h1 className=' text-3xl font-bold'>Document Upload</h1>
+      <hr />
+      <FileUploader />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/src/components/TitleHeader.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TitleHeader = (title: string) => {
+  return (
+    <div>
+      <h1 className=' text-3xl font-bold'>{title}</h1>
+    </div>
+  )
+}
+
+export default TitleHeader
