efd385de81ac2c8eefb6ffd46cce762b772df283
Yumenio <alejandroandy447@gmail.com>
2024-02-27 16:08:01 -0500

    feat: adds ElapseData component from LeftPane

--- src/src/components/LeftPane/ElapseDataItems.tsx
+++ src/src/components/LeftPane/ElapseDataItems.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PaneSectionHeader from '../Common/PaneSectionHeader'
 
 const ElapseDataItems = () => {
+  const [data, setData] = useState<null | string[]>(null)
   return (
     <div>
       <PaneSectionHeader title={"Elapse Data Checking"} />
+      {data? (
+        <ul>
+          {data.map((item, index) => <li key={index}>{item}</li>)}
+        </ul>
+      ):(
+        <p className="text-green-500 font-semibold">No Elapsed Dates!</p>
+      )}
     </div>
   )
 }
