0c788f02a5c9164e97001c1c0ab37720d5c90489
Yumenio <alejandroandy447@gmail.com>
2024-02-27 14:20:29 -0500

    feat: adds Tolerance Window logic and styling

--- src/src/components/LeftPane/ToleranceWindow.tsx
+++ src/src/components/LeftPane/ToleranceWindow.tsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {FaClock} from 'react-icons/fa';
+import PaneSectionHeader from '../Common/PaneSectionHeader'
 
 const ToleranceWindow = () => {
+  const [toggleOn, setToggleOn] = useState(false);
+  const [selectedDay, setSelectedDay] = useState('');
+
+  const handleToggle = () => {
+    setToggleOn(!toggleOn);
+  }
+
+  const handleSelectDay = () => {
+    // TODO
+  }
   return (
-    <div>ToleranceWindow</div>
+    <div>
+      <PaneSectionHeader title="Tolerance Window:" />
+      <div className="flex flex-col items-center sm:flex-row sm:items-center space-y-2 sm:space-y-0 sm:space-x-4 mt-2">
+        <div className={`rounded-full w-16 h-8 flex bg-${!toggleOn ? 'gray-300' : 'skyhopBlue'} p-1 cursor-pointer mb-2 sm:mb-0`} onClick={handleToggle}>
+          <div className={`w-6 h-6 rounded-full bg-white transform transition-transform ${toggleOn ? 'translate-x-8' : ''}`}>
+          </div>
+        </div>
+        <p className="mr-2">{toggleOn ? 'Toggle ON' : 'Toggle OFF'}</p>
+        <span className="hidden sm:inline">|</span>
+        <button
+          className={`flex items-center py-2 px-4 rounded ${!toggleOn ? 'cursor-not-allowed opacity-50' : ''}`}
+          onClick={handleSelectDay}
+          disabled={!toggleOn}
+        >
+          <FaClock className={`text-${toggleOn ? 'green' : 'red'}-500 mr-2`} />
+          Select Tolerance Level
+        </button>
+      </div>
+    </div>
   )
 }
 
