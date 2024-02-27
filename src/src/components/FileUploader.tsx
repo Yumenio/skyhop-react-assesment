54be04ed482d94daec33b8a0bb2f3d5ac250a914
Yumenio <alejandroandy447@gmail.com>
2024-02-27 12:01:20 -0500

    feat: adds outer box to fileUploader component

--- src/src/components/FileUploader.tsx
+++ src/src/components/FileUploader.tsx
@@ -14,40 +14,42 @@ const FileUploader: React.FC = () => {
 
   return (
     <div className='flex flex-col items-center mt-4'>
-      <div
-        {...getRootProps()}
-        className={`w-[400px] h-[200px] border-dashed border-2 rounded-lg border-gray-300 p-4 cursor-pointer relative flex items-center justify-center text-center ${isDragActive ? 'border-blue-500' : ''}`}
-      >
-        <input {...getInputProps()} />
-        {uploadedFileName ? (
-          <p className='bottom-2 left-2 bg-white text-gray-500 px-2 py-1 rounded'>
-            {uploadedFileName}
-          </p>
-        ) : isDragActive ? (
-          <p>Drop the file here</p>
-        ) : (
-          <p>
-            Drag & Drop Here, or <b>Browse</b>
-          </p>
-        )}
+      <div className="border-2 rounded-lg w-[400px] h-[300px]">
+        <div
+          {...getRootProps()}
+          className={`border-dashed border-2 rounded-lg border-gray-300 p-4 cursor-pointer relative flex items-center justify-center text-center ${isDragActive ? 'border-blue-500' : ''}`}
+        >
+          <input {...getInputProps()} />
+          {uploadedFileName ? (
+            <p className='bottom-2 left-2 bg-white text-gray-500 px-2 py-1 rounded'>
+              {uploadedFileName}
+            </p>
+          ) : isDragActive ? (
+            <p>Drop the file here</p>
+          ) : (
+            <p>
+              Drag & Drop Here, or <b>Browse</b>
+            </p>
+          )}
+        </div>
+        <button
+          className='mt-2 px-4 py-2 bg-skyhopBlue text-white cursor-pointer rounded-lg'
+          onClick={() => document.getElementById('fileInput')?.click()}
+        >
+          Upload Manifest
+        </button>
+        <input
+          id='fileInput'
+          type='file'
+          className='hidden'
+          onChange={(e) => {
+            const file = e.target.files?.[0]
+            if (file) {
+              setUploadedFileName(file.name)
+            }
+          }}
+        />
       </div>
-      <button
-        className='mt-2 px-4 py-2 bg-skyhopBlue text-white cursor-pointer rounded-lg'
-        onClick={() => document.getElementById('fileInput')?.click()}
-      >
-        Upload Manifest
-      </button>
-      <input
-        id='fileInput'
-        type='file'
-        className='hidden'
-        onChange={(e) => {
-          const file = e.target.files?.[0]
-          if (file) {
-            setUploadedFileName(file.name)
-          }
-        }}
-      />
     </div>
   )
 }
