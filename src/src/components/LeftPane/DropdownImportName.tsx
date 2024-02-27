c6b860e16e188fdf5afd66a2e2e6590b0f45775e
Yumenio <alejandroandy447@gmail.com>
2024-02-27 16:07:29 -0500

    feat: adds dropdown component from left pane

--- src/src/components/LeftPane/DropdownImportName.tsx
+++ src/src/components/LeftPane/DropdownImportName.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import Select from 'react-select';
 
 const DropdownImportName = () => {
+  const importNameOptions = [
+    {value: 'Option1', label: 'Option1'},
+    {value: 'Option2', label: 'Option2'},
+  ];
   return (
-    <div>DropdownImportName</div>
+    <div className="w-11/12">
+      <Select
+        options={importNameOptions}
+        placeholder="Select Import Name:"
+      />
+    </div>
   )
 }
 
